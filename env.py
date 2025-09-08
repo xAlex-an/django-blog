207f3b7fa21d473a2baa26cbe9eabbb85456a216
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-08 19:06:11 +0100

    Remove env.py to eliminate hardcoded database credentials

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_VYQ7RlhnGCW5@ep-shiny-recipe-ag6hqgp7.c-2.eu-central-1.aws.neon.tech/thus_sushi_shirt_840450"
-)

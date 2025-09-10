510e27ad62e3d48e704ea7cc3c97fdab6d5c7db3
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-10 13:53:58 +0100

    Implement PostList view and update URL routing for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+
 
 # Create your views here.
-
-def my_blog(request):
-    return HttpResponse("Hello, blog!")
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
+    

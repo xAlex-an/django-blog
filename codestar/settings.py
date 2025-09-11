c33db0bc81a66a453abd52b4940b4356e7145ccc
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-11 11:21:59 +0100

    Update TEMPLATES_DIR setting to include templates directory in Django settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,8 +15,10 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
